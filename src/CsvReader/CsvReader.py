690bbe8fe0e9054c86bb2decda75a66b44986c32
Nayana Kumari <70977839+nt27web@users.noreply.github.com>
2020-11-01 01:09:01 -0400

    Feature: Csv reader feature and test cases created.

---
+++ src/CsvReader/CsvReader.py
@@ -0,0 +1,24 @@
+import csv
+from Fileutilities.absolutepath import absolute_path
+
+
+def class_factory(class_name, dictionary):
+    return type(class_name, (object,), dictionary)
+
+
+class CsvReader:
+    data = []
+
+    def __init__(self, filepath):
+        self.data = []
+        with open(absolute_path(filepath)) as text_data:
+            csv_data = csv.DictReader(text_data, delimiter=',')
+            for row in csv_data:
+                self.data.append(row)
+        pass
+
+    def return_data_as_class(self, class_name):
+        objects = []
+        for row in self.data:
+            objects.append(class_factory(class_name, row))
+        return objects
